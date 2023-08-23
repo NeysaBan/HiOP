dbebb325e4dd94fa1fc54db353c516bcfd2beb66
NeysaBan <51215901123@stu.ecnu.edu.cn>
2023-08-23 09:30:01 +0000

    delete model test demo from pytest case

--- case/pytest/reduce_test.py
+++ case/pytest/reduce_test.py
@@ -1,5 +1,6 @@
 import yaml
 import torch
+import torch.nn as nn
 import pytest
 import numpy as np
 from rectime import show_time
@@ -7,7 +8,7 @@ from torch.autograd import Function
 from torch.utils.cpp_extension import load
 
 
-with open("unit_test/pyconfig.yaml", 'r', encoding='utf-8') as f:
+with open("case/pytest/pyconfig.yaml", 'r', encoding='utf-8') as f:
     config = yaml.safe_load(f)
 device = 'cuda:' + str(config['device_id'])
 
@@ -16,30 +17,18 @@ cuda_module = load(name="reduce_arr",
                         sources=["reg_torch/reduce_op.cpp", "kernel/reduce_kernel.cu"],
                         verbose=True)
 
+## test data
 N = 100000
 input = torch.rand(size=(1, N), dtype=torch.float32).to(device)
 output = torch.empty(1).to(device)
 doutput = torch.empty(1).to(device)
 
-# class cudaReduce(Function):
-#     @staticmethod
-#     def forward(ctx, output, input, n, device):
-
-#         c = torch.empty(n).to(device)
-#         cuda_module.torch_launch_reduce(output, input, n)
-
-#         return c 
-
-#     @staticmethod
-#     def backward(ctx: Any, *grad_outputs: Any) -> Any:
-
-#         return super().backward(ctx, *grad_outputs)
-
+def torch_go():
+    output = torch.sum(input)
+    
 def cuda_go():
     cuda_module.torch_launch_reduce(doutput, input, N)
 
-def torch_go():
-    output = torch.sum(input)
 
 class Test_reduce:
     def test_torch(self):
